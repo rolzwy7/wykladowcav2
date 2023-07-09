8eecfc0c930e87175eead4b5283a9340c5b13007
Bartosz Nowakowski <rolzwy7@gmail.com>
2023-07-09 12:08:53 +0200

    Modified settings resolution for fakturownia.pl

--- src/wykladowcapl/settings/fakturownia.py
+++ src/wykladowcapl/settings/fakturownia.py
@@ -1,7 +1,12 @@
-from .base_profile import DEBUG
+from .base_profile import APP_ENV
 
-if DEBUG:
+if APP_ENV == "develop":
     FAKTUROWNIA_API_URL = "https://rolzwy7.fakturownia.pl"
     FAKTUROWNIA_API_KEY = "FI6fTUUD8Pfjg7nFwG1M"
-else:
-    ...  # TODO: Set Fakturownia's settings
+elif APP_ENV == "production":
+    ...
+elif APP_ENV == "staging":
+    FAKTUROWNIA_API_URL = "https://rolzwy7.fakturownia.pl"
+    FAKTUROWNIA_API_KEY = "FI6fTUUD8Pfjg7nFwG1M"
+elif APP_ENV == "testing":
+    ...
