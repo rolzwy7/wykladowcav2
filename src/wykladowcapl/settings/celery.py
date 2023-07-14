d8aa81df5aa18887bfc1a4c276a70f5b20cdf80a
Bartosz Nowakowski <rolzwy7@gmail.com>
2023-07-14 12:18:04 +0200

    Added `CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP` setting
    Celery 5.* depracation warning before Celery 6.* release
    CPendingDeprecationWarning: The broker_connection_retry configuration
    setting will no longer determine whether broker connection retries are
    made during startup in Celery 6.0 and above. If you wish to retain the
    existing behavior for retrying connections on startup, you should set
    `broker_connection_retry_on_startup` to True.

--- src/wykladowcapl/settings/celery.py
+++ src/wykladowcapl/settings/celery.py
@@ -6,9 +6,13 @@ CELERY_RESULT_EXTENDED = True
 CELERY_TIMEZONE = "Europe/Warsaw"
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 
-# CELERY_TASK_TRACK_STARTED = True
-# CELERY_TASK_TIME_LIMIT = 30 * 60
-
+# Celery 5.* depracation warning before Celery 6.* release
+# CPendingDeprecationWarning: The broker_connection_retry configuration
+# setting will no longer determine whether broker connection retries are
+# made during startup in Celery 6.0 and above. If you wish to retain the
+# existing behavior for retrying connections on startup, you should set
+# `broker_connection_retry_on_startup` to True.
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 
 if APP_ENV == "develop":
     CELERY_BROKER_URL = "redis://redis:6379/5"
