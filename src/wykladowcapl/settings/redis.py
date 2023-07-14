5d65f7883a76b311cc58e82f621d17c8940fba5f
Bartosz Nowakowski <rolzwy7@gmail.com>
2023-07-14 12:12:33 +0200

    Fixed typo error in redis.py (settings)
    `REDIS_DB` got `REDIS_USER` env key instead of `REDIS_DB`

--- src/wykladowcapl/settings/redis.py
+++ src/wykladowcapl/settings/redis.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-REDIS_DB = os.environ.get("REDIS_USER", "0")
+REDIS_DB = os.environ.get("REDIS_DB", "0")
 REDIS_USER = os.environ.get("REDIS_USER")
 REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")  # Secret
 REDIS_HOST = os.environ.get("REDIS_HOST")
