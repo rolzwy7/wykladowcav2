01c0cb97044b0eabc69f1cf4d784f29f8611605b
Bartosz Nowakowski <rolzwy7@gmail.com>
2023-07-28 21:14:46 +0200

    Improved PDF application card

--- src/core/services/application_pdf_card_service.py
+++ src/core/services/application_pdf_card_service.py
@@ -10,6 +10,7 @@ from borb.pdf import (
     Document,
     FlexibleColumnWidthTable,
     HexColor,
+    Image,
     Page,
     PageLayout,
     Paragraph,
@@ -20,7 +21,9 @@ from borb.pdf.canvas.font.simple_font.true_type_font import TrueTypeFont
 from borb.pdf.canvas.layout.layout_element import LayoutElement
 from django.conf import settings
 from django.template.defaultfilters import date as _date
+from django.urls import reverse
 
+from core.consts.exemptions_consts import PRICE_ADNOTATION
 from core.models import (
     WebinarApplication,
     WebinarApplicationCompany,
@@ -32,7 +35,9 @@ COMPANY_NAME = settings.COMPANY_NAME
 COMPANY_NAME_FULL = settings.COMPANY_NAME_FULL
 COMPANY_OFFICE_EMAIL = settings.COMPANY_OFFICE_EMAIL
 BASE_DIR = settings.BASE_DIR
+BASE_URL = settings.BASE_URL
 ASSETS_DIR: Path = BASE_DIR.parent / "core" / "assets"
+LOGO_PATH: Path = ASSETS_DIR / "pdf_card" / "logo_pdf_card.png"
 
 
 class PdfCardFonts:
@@ -63,7 +68,7 @@ class ApplicationPdfCardService:
         )
 
         self.number_of_columns = 12
-        self.number_of_rows = 13
+        self.number_of_rows = 14
 
         self.number_of_rows += self.application.participants.count()
 
@@ -337,10 +342,28 @@ class ApplicationPdfCardService:
         """Create PDF application card"""
 
         self.layout.add(
+            Image(
+                LOGO_PATH,
+                width=Decimal(150),
+                height=Decimal(25),
+            )
+        )
+
+        self.layout.add(
             Paragraph(
                 f"Karta Zgłoszeniowa - {COMPANY_NAME}\n"
                 "Prosimy o odesłanie podpisanej karty"
                 f" na adres e-mail: {COMPANY_OFFICE_EMAIL}",
+                font=self.font_bold,
+                horizontal_alignment=Alignment.CENTERED,
+                text_alignment=Alignment.CENTERED,
+                respect_newlines_in_text=True,
+            )
+        )
+
+        self.layout.add(
+            Paragraph(
+                f"Zgłoszenie numer: {self.application.uuid}",
                 font=self.font_regular,
                 horizontal_alignment=Alignment.CENTERED,
                 text_alignment=Alignment.CENTERED,
@@ -398,7 +421,7 @@ class ApplicationPdfCardService:
         self.table.add(
             TableCell(
                 Paragraph(
-                    f"{self.application.total_price_netto} zł",
+                    f"{self.application.total_price_netto} zł {PRICE_ADNOTATION}",
                     font=self.font_regular,
                 ),
                 column_span=3,
@@ -421,13 +444,56 @@ class ApplicationPdfCardService:
         if submitter:
             self._add_submitter_row(submitter)
 
+        invoice: WebinarApplicationInvoice = self.application.invoice  # type: ignore
+        vat_exemption_display = invoice.get_vat_exemption_display()  # type: ignore
+
+        self.table.add(
+            TableCell(
+                Paragraph(
+                    "E-mail (faktura):",
+                    font=self.font_bold,
+                ),
+                column_span=4,
+                background_color=HexColor("D3D3D3"),
+            )
+        )
+        self.table.add(
+            TableCell(
+                Paragraph(
+                    invoice.invoice_email,
+                    font=self.font_regular,
+                ),
+                column_span=8,
+            )
+        )
+
+        self.table.add(
+            TableCell(
+                Paragraph(
+                    "Zwolnienie z VAT:",
+                    font=self.font_bold,
+                ),
+                column_span=4,
+                background_color=HexColor("D3D3D3"),
+            )
+        )
+        self.table.add(
+            TableCell(
+                Paragraph(
+                    vat_exemption_display,
+                    font=self.font_regular,
+                ),
+                column_span=8,
+            )
+        )
+
         self.table.add(
             TableCell(
                 Paragraph(
                     "Dodatkowe uwagi:",
                     font=self.font_bold,
                 ),
-                column_span=6,
+                column_span=4,
                 background_color=HexColor("D3D3D3"),
             )
         )
@@ -437,34 +503,109 @@ class ApplicationPdfCardService:
                     self.application.additional_information or "Brak",
                     font=self.font_regular,
                 ),
-                column_span=6,
+                column_span=8,
             )
         )
 
-        invoice: WebinarApplicationInvoice = self.application.invoice  # type: ignore
-
-        if invoice:
-            self.table.add(
-                TableCell(
-                    Paragraph(
-                        "E-mail (faktura):",
-                        font=self.font_bold,
-                    ),
-                    column_span=6,
-                    background_color=HexColor("D3D3D3"),
-                )
-            )
-            self.table.add(
-                TableCell(
-                    Paragraph(
-                        invoice.invoice_email,
-                        font=self.font_regular,
-                    ),
-                    column_span=6,
-                )
-            )
-
         self.layout.add(self.table)
+
+        self.layout.add(
+            Paragraph(
+                " ".join(
+                    [
+                        "Przesłanie karty zgłoszenia stanowi potwierdzenie",
+                        "przyjęcia oferty oraz potwierdzenie zapoznania się z",
+                        "regulaminem świadczenia usług szkoleniowych",
+                        "dostępnym na stronie",
+                        f"{BASE_URL}{reverse('core:terms_and_conditions_webinars')}",
+                    ]
+                ),
+                font=self.font_regular,
+                horizontal_alignment=Alignment.LEFT,
+                text_alignment=Alignment.LEFT,
+                respect_newlines_in_text=True,
+            )
+        )
+
+        table_signatures = FlexibleColumnWidthTable(
+            number_of_columns=12,
+            number_of_rows=2,
+            border_top=False,
+            border_right=False,
+            border_bottom=False,
+            border_left=False,
+        )
+
+        table_signatures.add(
+            TableCell(
+                Paragraph(
+                    "\n\n\n",
+                    font=self.font_bold,
+                    respect_newlines_in_text=True,
+                ),
+                column_span=4,
+            )
+        )
+
+        table_signatures.add(
+            TableCell(
+                Paragraph(
+                    "\n\n\n",
+                    font=self.font_bold,
+                    respect_newlines_in_text=True,
+                ),
+                column_span=4,
+            )
+        )
+
+        table_signatures.add(
+            TableCell(
+                Paragraph(
+                    "\n\n\n",
+                    font=self.font_bold,
+                    respect_newlines_in_text=True,
+                ),
+                column_span=4,
+            )
+        )
+
+        table_signatures.add(
+            TableCell(
+                Paragraph(
+                    "Pieczątka firmowa",
+                    font=self.font_bold,
+                    horizontal_alignment=Alignment.CENTERED,
+                    text_alignment=Alignment.CENTERED,
+                ),
+                column_span=4,
+            )
+        )
+
+        table_signatures.add(
+            TableCell(
+                Paragraph(
+                    "Miejscowość, data",
+                    font=self.font_bold,
+                    horizontal_alignment=Alignment.CENTERED,
+                    text_alignment=Alignment.CENTERED,
+                ),
+                column_span=4,
+            )
+        )
+
+        table_signatures.add(
+            TableCell(
+                Paragraph(
+                    "Podpis osoby upoważnionej",
+                    font=self.font_bold,
+                    horizontal_alignment=Alignment.CENTERED,
+                    text_alignment=Alignment.CENTERED,
+                ),
+                column_span=4,
+            )
+        )
+
+        self.layout.add(table_signatures)
 
         return self
 
