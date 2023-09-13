6729831ddf6447363bdd4788706e5425f0e2eef3
Bartosz Nowakowski <rolzwy7@gmail.com>
2023-09-13 20:44:53 +0200

    Added postgres db to docker compose
    - moved django debug toolbar from dev dependencies

--- src/wykladowcapl/settings/database.py
+++ src/wykladowcapl/settings/database.py
@@ -1,11 +1,24 @@
-from .base_profile import BASE_DIR
+from .base_profile import APP_ENV, BASE_DIR
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if APP_ENV == "production":
+    ...
+elif APP_ENV == "staging":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "OPTIONS": {
+                "service": "postgres_db_service",
+                "passfile": ".pgpass",
+            },
+        }
     }
-}
+elif APP_ENV == "develop":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
