9895664e5f0dc06815479c1bcb825a4c4c67622c
Bartosz Nowakowski <rolzwy7@gmail.com>
2024-06-30 16:04:06 +0200

    fix contributor access

--- src/core/views/recording/recording_streaming.py
+++ src/core/views/recording/recording_streaming.py
@@ -1,3 +1,9 @@
+"""
+Recording streaming chunk page
+"""
+
+# flake8: noqa=E501
+
 from django.core.exceptions import PermissionDenied
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404
@@ -12,6 +18,8 @@ def recording_streaming(request: HttpRequest, uuid: str):
     # Get recording token
     recording_token = get_object_or_404(WebinarRecordingToken, token=uuid)
     streaming_service = StreamingService(recording_token)
+    if request.user.is_authenticated and request.user.contributor:  # type: ignore
+        streaming_service.set_is_contributor(True)
 
     if not streaming_service.is_token_valid():
         raise PermissionDenied()
