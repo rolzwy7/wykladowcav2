e84f6a05bdd30fbda9008505e67acc4caeee7ff6
Bartosz Nowakowski <rolzwy7@gmail.com>
2024-07-23 19:03:04 +0200

    update

--- src/core/views/crm/application/actions/__init__.py
+++ src/core/views/crm/application/actions/__init__.py
@@ -1,3 +1,4 @@
+from .crm_application_resend_confirmation import crm_application_resend_confirmation
 from .crm_application_send_invoice_action import (
     crm_application_send_invoice_action_page,
 )
