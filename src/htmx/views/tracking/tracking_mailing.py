f6c308a2bf94d022085b6ff10fcbc791848dca82
Bartosz Nowakowski <rolzwy7@gmail.com>
2025-03-29 12:21:21 +0100

    update

--- src/htmx/views/tracking/tracking_mailing.py
+++ src/htmx/views/tracking/tracking_mailing.py
@@ -1,14 +1,13 @@
 """Tracking mailing"""
 
-import re
+# flake8: noqa=E501
 
+from django.core.cache import cache
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 from django.views.decorators.cache import cache_page
 
 from core.libs.mongo.db import MongoDBClient
-from core.models import Webinar
 from core.models.mailing import MailingCampaign
 from core.services.mailing import MailingTrackingService
 
@@ -25,11 +24,21 @@ def tracking_mailing(request, tracking_code: str):
 def tracking_mailing_clicks(request, webinar_id: int):
     """tracking_mailing_clicks"""
 
+    # Check cache
+    cache_key = f"CACHED-TRACKING-MAILING-CLICKS-WEBINAR-{webinar_id}"
+    cache_seconds = 60 * 10  # 10 minutes
+    if cache.get(cache_key):
+        return TemplateResponse(
+            request,
+            "htmx/tracking_mailing_clicks.html",
+            {"per_campaign": cache.get(cache_key)},
+        )
+
     _, database = MongoDBClient.get_connection()
 
     click_docs = database["wykladowcav2_mailing_clicks"].find(
-        # {"request_url": {"$regex": f"szkl/{webinar_id}"}}
-        {"request_url": {"$regex": f"szkl/506"}}
+        {"request_url": {"$regex": f"szkl/{webinar_id}"}}
+        # {"request_url": {"$regex": f"szkl/506"}}
     )
 
     per_campaign: dict[int, dict] = {}
@@ -40,16 +49,16 @@ def tracking_mailing_clicks(request, webinar_id: int):
         if campaign_id in per_campaign:
             per_campaign[campaign_id]["click_count"] += 1
         else:
-            per_campaign[campaign_id] = {"click_count": 1}
-
-    # # Check cache
-    # cache_key = f"CACHED-CAMPAIGN-COUNT-{campaign_id}-{pool_item_status}"
-    # cache_seconds = 15  # 15 seconds
-    # if cache.get(cache_key):
-    #     count = cache.get(cache_key)
-    #     return HttpResponse(f"{count:,}", content_type="text/plain; charset=utf8")
+            per_campaign[campaign_id] = {
+                "click_count": 1,
+                "campaign": MailingCampaign.manager.filter(id=campaign_id).first(),
+            }
 
     # Set cache
-    # cache.set(cache_key, count, timeout=cache_seconds)
+    cache.set(cache_key, per_campaign, timeout=cache_seconds)
 
-    return HttpResponse(str(per_campaign), content_type="text/plain; charset=utf8")
+    return TemplateResponse(
+        request,
+        "htmx/tracking_mailing_clicks.html",
+        {"per_campaign": per_campaign},
+    )
