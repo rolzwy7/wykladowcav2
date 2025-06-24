21b0a3e085b9fe9455a733524b70e99164a3f09a
Bartosz Nowakowski <rolzwy7@gmail.com>
2025-06-24 18:01:17 +0200

    update

--- src/core/libs/mailing/test_email.py
+++ src/core/libs/mailing/test_email.py
@@ -5,6 +5,7 @@
 from django.conf import settings
 from django.urls import reverse
 
+from core.libs.mailing.title_test import get_or_create_mailing_title_test
 from core.models import MailingCampaign, MailingTemplate
 from core.services import SenderSmtpService
 from core.services.mailing import MailingResignationService, MailingTrackingService
@@ -33,6 +34,8 @@ def send_campaign_test_email(email: str, mailing_campaign: MailingCampaign):
     tracking_code = MailingTrackingService.get_or_create_tracking(email)
     subject = f"[PRÓBA] {mailing_campaign.get_random_subject()}"
 
+    test_title = get_or_create_mailing_title_test(subject, str(campaign_id))
+
     with service.get_smtp_connection() as connection:
         service.send_email(
             connection=connection,
@@ -44,4 +47,5 @@ def send_campaign_test_email(email: str, mailing_campaign: MailingCampaign):
             resignation_url=resignation_url,
             tracking_code=tracking_code,
             campaign_id=campaign_id,
+            test_subject_id=str(test_title.id),  # type: ignore
         )
