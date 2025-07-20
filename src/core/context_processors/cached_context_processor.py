16426207b7385e0d9f08362b11207d584c07b2ce
Bartosz Nowakowski <rolzwy7@gmail.com>
2025-07-20 12:51:54 +0200

    update

--- src/core/context_processors/cached_context_processor.py
+++ src/core/context_processors/cached_context_processor.py
@@ -5,9 +5,16 @@ Consts context processor
 # flake8: noqa=E501
 
 from django.core.cache import cache
-from django.utils.timezone import now
+from django.template.defaultfilters import date as _date
+from django.urls import reverse
+from django.utils.timezone import get_default_timezone, now
 
-from core.models import ServiceOfferApplication, Webinar, WebinarCategory
+from core.models import (
+    ServiceOfferApplication,
+    Webinar,
+    WebinarAggregate,
+    WebinarCategory,
+)
 
 
 def cached(request):
@@ -55,6 +62,74 @@ def cached(request):
         cached_dict[cache_key] = ServiceOfferApplication.manager.filter(
             created_at__date=now().date()
         ).count()
+        cache.set(cache_key, cached_dict[cache_key], timeout=cache_seconds)
+
+    # Get service applications sent today
+    cache_key = "CACHED_MEGAMENU"
+    cache_seconds = 60 * 60  # 1 hour
+    if cache.get(cache_key):
+        cached_dict[cache_key] = cache.get(cache_key)
+    else:
+        menu_data = {}
+
+        def get_megamenu_result(aggregate: WebinarAggregate):
+            """get_megamenu_result"""
+            tz = get_default_timezone()
+            dates = []
+
+            for webinar in aggregate.webinars.all():
+                if webinar.is_active:
+                    dates.append(
+                        f"{_date(webinar.date.astimezone(tz), 'j E Y')} godz. {_date(webinar.date.astimezone(tz), 'H:i')}"
+                    )
+
+            return {
+                "title": aggregate.title,
+                "url": reverse(
+                    "core:webinar_aggregate_page", kwargs={"slug": aggregate.slug}
+                ),
+                "avatar": f"/media/uploads/lecturers/{aggregate.lecturer.slug}_80x80.webp" if not aggregate.is_anonymized else "/static/default/default-lecturer-avatar-80x80.webp",  # type: ignore
+                "lecturer_fullname": aggregate.lecturer.fullname if not aggregate.is_anonymized else "",  # type: ignore
+                "dates": dates,
+                "price": f"{aggregate.minimal_price}",
+            }
+
+        for main_cat in WebinarCategory.manager.get_main_categories():
+            subcategories = []
+
+            for _ in WebinarCategory.manager.get_subcategories(main_cat):
+                any_aggregates = (
+                    WebinarAggregate.manager.get_active_aggregates_for_category_slugs(
+                        slugs=[_.slug]
+                    )
+                )
+                if any_aggregates:
+                    subcategories.append(_.name)
+
+            menu_data[f"{main_cat.name_homepage}"] = {
+                "subcategories": subcategories,
+                "results": {
+                    "Wszystko": [
+                        get_megamenu_result(agg)
+                        for agg in WebinarAggregate.manager.get_active_aggregates_for_category_slugs(
+                            slugs=[main_cat.slug]
+                        )
+                    ],
+                    **{
+                        f"{subcat.name}": [
+                            get_megamenu_result(agg)
+                            for agg in WebinarAggregate.manager.get_active_aggregates_for_category_slugs(
+                                slugs=[subcat.slug]
+                            )
+                        ]
+                        for subcat in WebinarCategory.manager.get_subcategories(
+                            main_cat
+                        )
+                    },
+                },
+            }
+
+        cached_dict[cache_key] = menu_data
         cache.set(cache_key, cached_dict[cache_key], timeout=cache_seconds)
 
     return cached_dict
